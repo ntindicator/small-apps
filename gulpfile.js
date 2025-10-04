b195fbb3d0e4c4a62e101cce7c674996cca770c7
Matthew Goss <30362690+ntindicator@users.noreply.github.com>
2025-10-04 10:26:54 +1000

    Update packages and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,122 +1,116 @@
 'use strict';
 
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var sourcemaps   = require('gulp-sourcemaps');
-var fileinclude  = require('gulp-file-include');
-var autoprefixer = require('gulp-autoprefixer');
-var runSequence  = require('run-sequence');
-var bs           = require('browser-sync').create();
-var rimraf       = require('rimraf');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const fileinclude = require('gulp-file-include');
+const autoprefixer = require('gulp-autoprefixer');
+const bs = require('browser-sync').create();
+const fs = require('fs');
+const path = require('path');
 
-var path = {
+
+const sapath = {
   src: {
-    html    : 'source/*.html',
-    others  : 'source/*.+(php|ico|png)',
-    htminc  : 'source/partials/**/*.htm',
-    incdir  : 'source/partials/',
-    plugins : 'source/plugins/**/*.*',
-    js      : 'source/js/*.js',
-    scss    : 'source/scss/**/*.scss',
-    images  : 'source/images/**/*.+(png|jpg|gif|svg)'
+    html: 'source/*.html',
+    others: 'source/*.+(php|ico|png)',
+    htminc: 'source/partials/**/*.htm',
+    incdir: 'source/partials/',
+    plugins: 'source/plugins/**/*.*',
+    js: 'source/js/*.js',
+    scss: 'source/scss/**/*.scss',
+    images: 'source/images/**/*.+(png|jpg|gif|svg)'
   },
   build: {
-    dirDev : 'theme/'
+    dirDev: 'theme/'
   }
 };
 
+// Clean Build Folder
+function clean(cb) {
+  const dir = path.resolve('./theme');
+  if (fs.existsSync(dir)) {
+    fs.rmSync(dir, { recursive: true, force: true });
+  }
+  cb();
+}
+
+
 // HTML
-gulp.task('html:build', function () {
-  return gulp.src(path.src.html)
-    .pipe(fileinclude({
-      basepath: path.src.incdir
-    }))
-    .pipe(gulp.dest(path.build.dirDev))
-    .pipe(bs.reload({
-      stream: true
-    }));
-});
+function html() {
+  return gulp.src(sapath.src.html)
+    .pipe(fileinclude({ basepath: sapath.src.incdir }))
+    .pipe(gulp.dest(sapath.build.dirDev))
+    .pipe(bs.stream());
+}
 
 // SCSS
-gulp.task('scss:build', function () {
-return gulp.src(path.src.scss)
-  .pipe(sourcemaps.init())
-  .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-  .pipe(autoprefixer())
-  .pipe(sourcemaps.write('/'))
-  .pipe(gulp.dest(path.build.dirDev + 'css/'))
-  .pipe(bs.reload({
-    stream: true
-  }));
-});
+function scss() {
+  return gulp.src(sapath.src.scss)
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(sapath.build.dirDev + 'css/'))
+    .pipe(bs.stream());
+}
 
 // Javascript
-gulp.task('js:build', function () {
-return gulp.src(path.src.js)
-  .pipe(gulp.dest(path.build.dirDev + 'js/'))
-  .pipe(bs.reload({
-    stream: true
-  }));
-});
+function js() {
+  return gulp.src(sapath.src.js)
+    .pipe(gulp.dest(sapath.build.dirDev + 'js/'))
+    .pipe(bs.stream());
+}
 
 // Images
-gulp.task('images:build', function () {
-return gulp.src(path.src.images)
-  .pipe(gulp.dest(path.build.dirDev + 'images/'))
-  .pipe(bs.reload({
-    stream: true
-  }));
-});
+function images() {
+  return gulp.src(sapath.src.images)
+    .pipe(gulp.dest(sapath.build.dirDev + 'images/'))
+    .pipe(bs.stream());
+}
 
 // Plugins
-gulp.task('plugins:build', function () {
-return gulp.src(path.src.plugins)
-  .pipe(gulp.dest(path.build.dirDev + 'plugins/'))
-  .pipe(bs.reload({
-    stream: true
-  }));
-});
+function plugins() {
+  return gulp.src(sapath.src.plugins)
+    .pipe(gulp.dest(sapath.build.dirDev + 'plugins/'))
+    .pipe(bs.stream());
+}
 
 // Other files like favicon, php, sourcele-icon on root directory
-gulp.task('others:build', function () {
-return gulp.src(path.src.others)
-  .pipe(gulp.dest(path.build.dirDev))
-});
-
-// Clean Build Folder
-gulp.task('clean', function (cb) {
-  rimraf('./theme', cb);
-});
+function others() {
+  return gulp.src(sapath.src.others)
+    .pipe(gulp.dest(sapath.build.dirDev));
+}
 
 // Watch Task
-gulp.task('watch:build', function () {
-  gulp.watch(path.src.html, ['html:build']);
-  gulp.watch(path.src.htminc, ['html:build']);
-  gulp.watch(path.src.scss, ['scss:build']);
-  gulp.watch(path.src.js, ['js:build']);
-  gulp.watch(path.src.images, ['images:build']);
-  gulp.watch(path.src.plugins, ['plugins:build']);
-});
+function watchFiles() {
+  gulp.watch(sapath.src.html, html);
+  gulp.watch(sapath.src.htminc, html);
+  gulp.watch(sapath.src.scss, scss);
+  gulp.watch(sapath.src.js, js);
+  gulp.watch(sapath.src.images, images);
+  gulp.watch(sapath.src.plugins, plugins);
+}
 
 // Build Task
-gulp.task('build', function () {
-  runSequence(
-    'clean',
-    'html:build',
-    'js:build',
-    'scss:build',
-    'images:build',
-    'plugins:build',
-    'others:build',
-    'watch:build',
-    function () {
-      bs.init({
-        server: {
-          baseDir: path.build.dirDev
-        }
-      });
-    }
-  );
-});
+const build = gulp.series(
+  clean,
+  gulp.parallel(html, js, scss, images, plugins, others)
+);
 
-gulp.task("default", ["build"]);
+function serve() {
+  bs.init({ server: { baseDir: sapath.build.dirDev } });
+  watchFiles();
+}
+
+exports.clean = clean;
+exports.html = html;
+exports.scss = scss;
+exports.js = js;
+exports.images = images;
+exports.plugins = plugins;
+exports.others = others;
+exports.watch = watchFiles;
+exports.build = build;
+exports.serve = gulp.series(build, serve);
+exports.default = exports.serve;
